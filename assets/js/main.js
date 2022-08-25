d69195cfae25e1f3ba2fa282d93176b6eca8045c
Rozandi1 <98866052+Rozandi1@users.noreply.github.com>
2022-08-25 21:39:18 +0800

    commit ke 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,8 +65,29 @@ tabs.forEach((tab) => {
     tab.classList.add("qualification__active");
   });
 });
-/*==================== SERVICES MODAL ====================*/
 
+/*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalClose = document.querySelectorAll(".services__modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalClose.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
 /*==================== PORTFOLIO SWIPER  ====================*/
 
 /*==================== TESTIMONIAL ====================*/
